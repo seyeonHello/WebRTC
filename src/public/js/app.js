c20b2bd1621718457335378472db2c780f00f892
seyeonHello <47855648+seyeonHello@users.noreply.github.com>
2021-11-22 13:57:06 +0900

    Update app.js

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -7,11 +7,35 @@ const room = document.getElementById("room");
 room.hidden=true;
 let roomName;
 
+function addMessage(message) {
+    const ul = room.querySelector("ul");
+    const li = document.createElement("li");
+    li.innerText = message;
+    ul.appendChild(li);
+}
+function handleMessageSubmit(event){
+    event.preventDefault();
+    const input=room.querySelector("#msg input");
+    const value=input.value;
+    socket.emit("new_message", input.value, roomName, ()=>{
+        addMessage(`You: ${value}`);
+    });
+    input.value='';
+}
+function handleNicknameSubmit(event){
+    event.preventDefault();
+    const input=room.querySelector("#name input");
+    socket.emit("nickname", input.value);
+}
 function showRoom() {
     welcome.hidden=true;
     room.hidden=false;
     const h3=room.querySelector("h3");
     h3.innerText=`Room ${roomName}`;
+    const msgForm=room.querySelector("#msg");
+    const nameForm=room.querySelector("#name");
+    msgForm.addEventListener("submit",handleMessageSubmit);
+    nameForm.addEventListener("submit",handleNicknameSubmit);
 }
 function handleRoomSubmit(event){
     event.preventDefault();
@@ -20,5 +44,28 @@ function handleRoomSubmit(event){
     roomName=input.value;
     input.value=""
 }
-
 form.addEventListener("submit",handleRoomSubmit);
+
+socket.on("welcome",(user, newCount)=>{
+    const h3=room.querySelector("h3");
+    h3.innerText=`ROOM ${roomName} (${newCount})`;
+    addMessage(`${user} arrived!`);
+});
+
+socket.on("bye",(left, newCount)=>{
+    const h3=room.querySelector("h3");
+    h3.innerText=`ROOM ${roomName} (${newCount})`;
+    addMessage(`${left} left !!`);
+})
+
+socket.on("new_message",addMessage);
+
+socket.on("room_change", (rooms)=>{
+    const roomList=welcome.querySelector("ul");
+    roomList.innerHTML="";
+    rooms.forEach((room)=>{
+         const li = document.createElement("li");
+         li.innerText=room;
+         roomList.append(li);
+    })
+});
